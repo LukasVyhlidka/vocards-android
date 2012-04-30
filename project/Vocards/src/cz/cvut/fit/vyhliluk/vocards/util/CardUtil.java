23ceda5d4471b5feaf6098671f9f13c72c453dc1
Lukas Vyhlidka <Lucky.V@gmail.com>
2012-04-30 14:16:19 +0200

    icon description

--- project/Vocards/src/cz/cvut/fit/vyhliluk/vocards/util/CardUtil.java
+++ project/Vocards/src/cz/cvut/fit/vyhliluk/vocards/util/CardUtil.java
@@ -1,7 +1,6 @@
 package cz.cvut.fit.vyhliluk.vocards.util;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
